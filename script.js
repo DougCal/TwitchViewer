176a8802fb2a227cf909150bd9593eac27719e5a
DougCal <drummer.john@live.com>
2016-09-22 10:18:21 -0500

    Did some cleaning

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var id = 'jhoj5nm5ftghejad68mctbx338152e5';
+var id = '?client_id=jhoj5nm5ftghejad68mctbx338152e5';
+var twitchAPI = 'https://api.twitch.tv/kraken/streams/';
 
 $(document).ready(function() {
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'freecodecamp' + id, function(data) {
     if (data.stream === null) {                     $("#fcc").prepend("<h2>offline</h2>");
     }else{
       $("#fccColor").css("background-color", "#4cff4c");
@@ -11,7 +12,7 @@ $(document).ready(function() {
   if($("#fcc").text())
     $("#fcc").prepend("<h2 class=text-center>FreeCodeCamp's account no longer exists</h2>");
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/gamegrumps?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'gamegrumps' + id, function(data) {
     if (data.stream === null) {                     $("#ggrumps").prepend("<h2>offline</h2>");
     }else{
       $("#ggColor").css("background-color", "#4cff4c");
@@ -21,7 +22,7 @@ $(document).ready(function() {
   if($("#ggrumps").text())
     $("#ggrumps").prepend("<h2 class=text-center>GameGrumps' account no longer exists</h2>");
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/medrybw?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'medrybw' + id, function(data) {
     if (data.stream === null) {                     $("#medrybw").prepend("<h2>offline</h2>");
     }else if(data.stream._id){
       $("#mbwColor").css("background-color", "#4cff4c");
@@ -31,7 +32,7 @@ $(document).ready(function() {
   if($("#medrybw").text())
     $("#medrybw").prepend("<h2 class=text-center>MedryBW's account no longer exists</h2>");
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/deadmau5?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'deadmau5' + id, function(data) {
     if (data.stream === null) {                     $("#deadmau5").prepend("<h2>offline</h2>");
     }else{
       $("#mau5Color").css("background-color", "#4cff4c");
@@ -41,7 +42,7 @@ $(document).ready(function() {
   if($("#deadmau5").text())
     $("#deadmau5").prepend("<h2 class=text-center>deadmau5's account no longer exists</h2>");
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/unrivaledtournaments?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'unrivaledtournaments' + id, function(data) {
     if (data.stream === null) {                    $("#unrivaledt").prepend("<h2>offline</h2>");
     }else{
      $("#unrivColor").css("background-color", "#4cff4c");
@@ -51,7 +52,7 @@ $(document).ready(function() {
   if($("#unrivaledt").text())
     $("#unrivaledt").prepend("<h2 class=text-center>UnrivaledTournaments' account no longer exists</h2>");
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/bifuteki?client_id=' + id, function(data) {
+  $.getJSON(twitchAPI + 'bifuteki' + id, function(data) {
     if (data.stream === null) {                    $("#bifuteki").prepend("<h2>offline</h2>");
     }else{
      $("#bifColor").css("background-color", "#4cff4c");
